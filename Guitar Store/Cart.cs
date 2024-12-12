251b4f0acc66ffb591af42ee16e1d9ba4ce425e2
Max <maksihnatyev.work@gmail.com>
2024-12-12 21:23:17 +0200

    Add delegates

--- Guitar Store/Cart.cs
+++ Guitar Store/Cart.cs
@@ -15,22 +15,76 @@ namespace Guitar_Store
 
         public Cart()
         {
-            throw new NotImplementedException();
+            Items = new List<Guitar>();
+            TotalPrice = 0m;
         }
+
+        public Action<Guitar> OnItemAdded { get; set; }
+        public Action<Guitar> OnItemRemoved { get; set; }
+        public Func<Guitar, bool> ItemDiscountCriteria { get; set; }
 
         public void AddItem(Guitar item)
         {
-            throw new NotImplementedException();
+            Items.Add(item);
+            TotalPrice += item.Price;
+
+            OnItemAdded?.Invoke(item);
         }
 
         public void RemoveItem(Guitar item)
         {
-            throw new NotImplementedException();
+            if (Items.Remove(item))
+            {
+                TotalPrice -= item.Price;
+
+                OnItemRemoved?.Invoke(item);
+            }
         }
 
         public void ClearCart()
         {
-            throw new NotImplementedException();
+            Items.Clear();
+            TotalPrice = 0m;
+        }
+
+        public void UpdateQuantity(Guitar item, int newQuantity)
+        {
+            if (newQuantity < 0) throw new ArgumentException("Кількість не може бути від'ємною.");
+
+            int currentQuantity = Items.Count(i => i.Id == item.Id);
+            int difference = newQuantity - currentQuantity;
+
+            if (difference > 0)
+            {
+                for (int i = 0; i < difference; i++)
+                {
+                    AddItem(item);
+                }
+            }
+            else if (difference < 0)
+            {
+                for (int i = 0; i < Math.Abs(difference); i++)
+                {
+                    RemoveItem(item);
+                }
+            }
+        }
+
+        public List<Guitar> FindItems(Predicate<Guitar> criteria)
+        {
+            return Items.FindAll(criteria);
+        }
+
+        public void ApplyDiscount(Func<Guitar, decimal> discountCalculator)
+        {
+            foreach (var item in Items)
+            {
+                if (ItemDiscountCriteria?.Invoke(item) == true)
+                {
+                    TotalPrice -= discountCalculator(item);
+                }
+            }
         }
     }
+
 }
